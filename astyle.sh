62d66d6c53a9bdaf41db57bd104ab06c371a1e84
heartyoh <heartyoh@hatiolab.com>
2016-06-25 21:23:34 +0900

    style

--- astyle.sh
+++ astyle.sh
@@ -1,1 +1,4 @@
+#!/bin/sh -x
+
 astyle -s2 --style=gnu --pad-header --align-pointer=name --indent-col1-comments --pad-first-paren-out --recursive src/*.c
+astyle -s2 --style=gnu --pad-header --align-pointer=name --indent-col1-comments --pad-first-paren-out --recursive src/*.h
